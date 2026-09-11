0fe584a1197113bf73974c1a719d19292d955b30
Andrew Cormier <andrew@cormierconsulting.com>
2026-09-11 01:58:14 -0500

    test: ignore generated reference outputs in brand scan (#81)

--- tests/Cormier.Realtime.KubernetesTests/BrandingConfigurationTests.cs
+++ tests/Cormier.Realtime.KubernetesTests/BrandingConfigurationTests.cs
@@ -11,9 +11,16 @@ public sealed partial class BrandingConfigurationTests
     {
         var excludedDirectories = new HashSet<string>(StringComparer.OrdinalIgnoreCase)
         {
-            ".backups", ".git", ".bootstrap", ".logs", "artifacts", "bin", "obj", "refs"
+            ".backups", ".bootstrap", ".build", ".git", ".gradle", ".logs", ".venv",
+            "_build", "artifacts", "bin", "deps", "node_modules", "obj", "refs", "target", "vendor"
         };
-        var candidates = Directory.EnumerateFiles(Root, "*", SearchOption.AllDirectories)
+        var enumeration = new EnumerationOptions
+        {
+            RecurseSubdirectories = true,
+            IgnoreInaccessible = true,
+            AttributesToSkip = FileAttributes.ReparsePoint
+        };
+        var candidates = Directory.EnumerateFiles(Root, "*", enumeration)
             .Where(path => !path.Split(Path.DirectorySeparatorChar).Any(excludedDirectories.Contains));
 
         var legacyBrand = string.Concat("pro", "pago");

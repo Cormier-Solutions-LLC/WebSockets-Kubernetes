cb471193f44b3bb7c43e44e9749385aa4aab660c
Andrew Cormier <andrew@cormierconsulting.com>
2026-09-13 09:17:21 -0500

    Stabilize PHP browser smoke on reused runners (#122)
    * stabilize php browser smoke on reused runners
    
    * run laravel cleanup before playwright startup
    
    * scope php smoke concurrency by ref

--- sdk/typescript/test/reference-adapters/stop-php-laravel-container.mjs
+++ sdk/typescript/test/reference-adapters/stop-php-laravel-container.mjs
@@ -1,5 +1,10 @@
 import { spawnSync } from "node:child_process";
+import { pathToFileURL } from "node:url";
 
 export default function stopPhpLaravelContainer() {
   spawnSync("docker", ["stop", "--timeout", "15", "cormier-php-laravel-playwright"], { stdio: "ignore" });
+}
+
+if (process.argv[1] && import.meta.url === pathToFileURL(process.argv[1]).href) {
+  stopPhpLaravelContainer();
 }
